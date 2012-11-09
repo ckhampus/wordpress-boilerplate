2038fedc5025b1cf9eafe9b89f328e4e1b642c01
Cristian Hampus <contact@cristianhampus.se>
2012-11-09 16:59:39 +0100

    Updated deploy config and composer dependencies.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,21 @@
 set :stages, %w(production staging development)
-set :default_stage, 'staging'
+set :default_stage, 'development'
 require 'capistrano/ext/multistage'
 
+# The application name.
 set :application, 'wordpress-boilerplate'
+set :user, 'www-data'
+set :group, 'www-data'
 
-set :repository, 'git@github.com:ckhampus/wordpress-boilerplate.git'
+# The application repository.
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `svn`, `mercurial`, `perforce`, `subversion` or `none`
+set :repository, 'git@github.com:ckhampus/wordpress-boilerplate.git'
 
 set :deploy_via, :remote_cache
 set :copy_exclude, ['.git', '.DS_Store', '.gitignore', '.gitmodules']
 set :git_enable_submodules, 1
 
-set :deploy_to, '/var/www'
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :deploy_to, "/var/www/#{application}"
 
 namespace :wordpress do
     desc 'Symlink the Uploads folder from shared folders'
